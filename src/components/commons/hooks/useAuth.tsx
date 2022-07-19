538302f8f9a6f1527732e2adebef42f9d2600a48
sehee-xx <didtpgml0627@gmail.com>
2022-07-19 14:34:31 +0900

    refreshToken 수정

--- src/components/commons/hooks/useAuth.tsx
+++ src/components/commons/hooks/useAuth.tsx
@@ -2,12 +2,9 @@ import { Modal } from "antd";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { getAccessToken } from "../../../commons/libraries/getAccessToken";
-// import { useRecoilValueLoadable } from "recoil";
-// import { restoreAccessTokenLoadable } from "../../../commons/store";
 
 export function useAuth() {
   const router = useRouter();
-  // const value = useRecoilValueLoadable(restoreAccessTokenLoadable);
 
   useEffect(() => {
     getAccessToken().then((newAccessToken) => {
@@ -21,17 +18,4 @@ export function useAuth() {
       }
     });
   }, []);
-
-  // useEffect(() => {
-  //   value.toPromise().then((newAccessToken) => {
-  //     if (!newAccessToken) {
-  //       Modal.error({
-  //         content: "관리자 계정으로 로그인 해주세요.",
-  //         onOk() {
-  //           router.push("/admin/login");
-  //         },
-  //       });
-  //     }
-  //   });
-  // }, []);
 }
