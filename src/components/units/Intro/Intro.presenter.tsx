89b29710c9461d283036ef35c0f4da4ff3a247c9
sehee-xx <didtpgml0627@gmail.com>
2022-07-19 17:53:56 +0900

    반응형 mobile

--- src/components/units/Intro/Intro.presenter.tsx
+++ src/components/units/Intro/Intro.presenter.tsx
@@ -12,7 +12,10 @@ export default function IntroUI() {
       </S.Header>
       <S.Body>
         <S.FirstPage>
-          <S.FirstTitle>매 순간마다 카픽과 함께하세요</S.FirstTitle>
+          <S.WebFirstTitle>매 순간마다 카픽과 함께하세요</S.WebFirstTitle>
+          <S.MobileFirstTitle>
+            <p>매 순간마다</p>카픽과 함께하세요
+          </S.MobileFirstTitle>
           <S.FirstArrow src="./images/ic_arrow.svg" />
         </S.FirstPage>
         <S.LineHr />
@@ -21,13 +24,9 @@ export default function IntroUI() {
             <S.SecondTitle>카픽으로 이웃의 차를 빌려보세요</S.SecondTitle>
             <S.SecondBottomDiv>
               <S.SecondContents>
-                물건은 더이상 ‘소유’의 개념이 아닌
-                <br />
-                서로 대여해 주고 차용해 쓰는 <br />
-                <strong style={{ fontSize: "30px", color: "#5d8bff" }}>
-                  공유
-                </strong>
-                의 개념이 되고 있습니다
+                <p>물건은 더이상 ‘소유’의 개념이 아닌</p>
+                <p>서로 대여해 주고 차용해 쓰는</p>
+                <p>공유의 개념이 되고 있습니다</p>
               </S.SecondContents>
               <S.SecondImage src="/images/bodyImg1.svg" />
             </S.SecondBottomDiv>
@@ -39,11 +38,9 @@ export default function IntroUI() {
             <S.ThirdTitle>평일에 차를 이용하지 않으세요?</S.ThirdTitle>
             <S.ThirdImage src="/images/bodyImg2.svg" />
             <S.ThirdContents>
-              차를 사용하는 날보다 사용하지 않는 날이 많다면! <br />
-              사용하지 않는 차량으로 돈을 벌고 싶다면! <br />
-              <strong style={{ color: "#5d8bff" }}>
-                지금 바로 카픽에서 오너등록 하세요!
-              </strong>
+              <p>차를 사용하지 않는 날이 많다면!</p>
+              <p>사용하지 않는 차량으로 돈을 벌고 싶다면!</p>
+              <p>지금 바로 카픽에서 오너등록 하세요!</p>
             </S.ThirdContents>
           </Fade>
         </S.ThirdPage>
@@ -53,32 +50,35 @@ export default function IntroUI() {
             <S.FourthTitle>카픽키로 손쉽고 간편하게!</S.FourthTitle>
             <S.FourthBottomDiv>
               <S.FourthContents>
-                스마트키를 사용해 키 분실 걱정이 없고
-                <br />
-                핸드폰으로 손쉽게 차량을 이용할 수 있습니다
-                <br />
-                <strong style={{ color: "#5d8bff" }}>
-                  지금 바로 카픽에서 체험해보세요!
-                </strong>
+                <p>스마트키를 사용해 키 분실 걱정이 없고</p>
+                <p>핸드폰으로 손쉽게 차량을 이용할 수 있습니다</p>
+                <p>지금 바로 카픽에서 체험해보세요!</p>
               </S.FourthContents>
               <S.FourthImage src="/images/bodyImg3.svg" />
             </S.FourthBottomDiv>
           </Fade>
         </S.FourthPage>
         <S.LineHr />
-        {/* 카픽과 함께 오너와 유저를 동시에 누려보세요. <br />
-          근처에 있는 다양한 차를 이용해 보세요. <br />
-          카픽은 여러분을 기다리고 있습니다! */}
       </S.Body>
       <S.Footer>
         <S.ButtonBox>
           <S.DownLoadButton>
             <S.ButtonImage src="/images/apple-logo.png"></S.ButtonImage>
-            <S.ButtonText>App Store</S.ButtonText>
+            <S.ButtonText
+              title="App Store 이동"
+              href="https://www.apple.com/kr/app-store/"
+            >
+              App Store
+            </S.ButtonText>
           </S.DownLoadButton>
           <S.DownLoadButton>
             <S.ButtonImage src="/images/google-play.png"></S.ButtonImage>
-            <S.ButtonText>Google Play</S.ButtonText>
+            <S.ButtonText
+              title="Google Play 이동"
+              href="https://play.google.com/"
+            >
+              Google Play
+            </S.ButtonText>
           </S.DownLoadButton>
         </S.ButtonBox>
       </S.Footer>
--- src/components/units/Intro/Intro.styles.ts
+++ src/components/units/Intro/Intro.styles.ts
@@ -16,7 +16,7 @@ const marginDiv = styled.div`
 `;
 
 const pageDiv = styled(marginDiv)`
-  height: calc(100vh - 75px);
+  height: 720px;
   display: flex;
 `;
 
@@ -48,7 +48,7 @@ export const Header = styled.div`
 
   @media ${breakPoints.mobile} {
     width: 100%;
-    height: 100px;
+    height: 80px;
   }
 `;
 
@@ -105,12 +105,12 @@ export const FirstPage = styled(pageDiv)`
   align-items: center;
 
   @media ${breakPoints.mobile} {
-    font-size: 30px;
+    font-size: 20px;
     font-weight: 700;
   }
 `;
 
-export const FirstTitle = styled.span`
+export const WebFirstTitle = styled.div`
   font-size: 55px;
   font-weight: 600;
   letter-spacing: 20px;
@@ -134,10 +134,39 @@ export const FirstTitle = styled.span`
   display: inline-block;
 
   @media ${breakPoints.mobile} {
-    width: 100%;
-    text-align: center;
-    font-size: 30px;
-    font-weight: 700;
+    display: none;
+  }
+`;
+
+export const MobileFirstTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  letter-spacing: 20px;
+  background-image: linear-gradient(
+    -225deg,
+    #294997 0%,
+    #4b75e0 32%,
+    #9e6bf1 66%,
+    #756bf1 80%,
+    #294997 100%
+  );
+  background-size: auto auto;
+  background-clip: border-box;
+  background-size: 200% auto;
+  color: #fff;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: inline-block;
+  width: 100%;
+  text-align: center;
+  text-justify: center;
+  padding-left: 20px;
+  animation: ${textclip} 1s linear infinite;
+
+  @media (min-width: 767px) {
+    display: none;
   }
 `;
 
@@ -158,49 +187,49 @@ export const SecondPage = styled(pageDiv)`
   padding: 0px 150px;
 
   @media ${breakPoints.mobile} {
-    flex-direction: column;
-    justify-content: center;
+    text-align: center;
     align-items: center;
+    padding: 0px 50px;
   }
 `;
 
 export const SecondTitle = styled.div`
   color: #5d8bff;
-  font-size: 50px;
+  font-size: 40px;
   font-weight: 600;
   letter-spacing: 10px;
-
-  /* @media ${breakPoints.tablet} {
-    color: #5d8bff;
-    font-size: 50px;
-    font-weight: 600;
-    letter-spacing: 10px;
-  } */
 
   @media ${breakPoints.mobile} {
     width: 100%;
     font-size: 30px;
-    padding-bottom: 20px;
+    padding-bottom: 50px;
   }
 `;
 
 export const SecondBottomDiv = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  @media ${breakPoints.mobile} {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export const SecondContents = styled.div`
   font-weight: normal;
-  font-size: 26px;
+  font-size: 20px;
   color: #0a0a0a;
-  line-height: 60px;
   letter-spacing: 3px;
   margin-top: 100px;
 
   @media ${breakPoints.mobile} {
+    margin: 0px 0px;
     width: 100%;
-    font-size: 10px;
+    font-size: 15px;
     padding-bottom: 20px;
   }
 `;
@@ -210,9 +239,9 @@ export const SecondImage = styled.img`
   height: auto;
   max-width: 500px;
 
-  @media ${breakPoints.tablet} {
-    max-width: 360px;
-    margin-top: 35px;
+  @media ${breakPoints.mobile} {
+    max-width: 350px;
+    display: flex;
   }
 `;
 
@@ -220,30 +249,55 @@ export const ThirdPage = styled(pageDiv)`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media ${breakPoints.mobile} {
+    text-align: center;
+    align-items: center;
+    padding: 0px 20px;
+  }
 `;
 
 export const ThirdTitle = styled.div`
   color: #5d8bff;
-  font-size: 50px;
+  font-size: 40px;
   font-weight: 600;
   letter-spacing: 10px;
+
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    font-size: 30px;
+    padding-bottom: 20px;
+  }
 `;
 
 export const ThirdImage = styled.img`
   width: 100%;
   height: auto;
-  max-width: 360px;
+  max-width: 350px;
   margin-top: 35px;
+
+  @media ${breakPoints.mobile} {
+    max-width: 300px;
+    display: flex;
+    padding-bottom: 50px;
+  }
 `;
 
 export const ThirdContents = styled.div`
+  width: 100%;
   font-weight: normal;
-  font-size: 26px;
+  font-size: 20px;
   text-align: end;
   color: #0a0a0a;
-  line-height: 45px;
   letter-spacing: 5px;
   margin-top: 50px;
+
+  @media ${breakPoints.mobile} {
+    text-align: center;
+    margin: 0px 0px;
+    width: 100%;
+    font-size: 15px;
+  }
 `;
 
 export const FourthPage = styled(pageDiv)`
@@ -251,25 +305,25 @@ export const FourthPage = styled(pageDiv)`
   justify-content: center;
   padding: 0px 150px;
 
-  @media ${breakPoints.tablet} {
-    flex-direction: column;
-    justify-content: center;
+  @media ${breakPoints.mobile} {
+    padding: 0px 50px;
     align-items: center;
+    justify-content: center;
   }
 `;
 
 export const FourthTitle = styled.div`
   color: #5d8bff;
   text-align: end;
-  font-size: 50px;
+  font-size: 40px;
   font-weight: 600;
   letter-spacing: 10px;
 
-  @media ${breakPoints.tablet} {
-    color: #5d8bff;
-    font-size: 50px;
-    font-weight: 600;
-    letter-spacing: 10px;
+  @media ${breakPoints.mobile} {
+    text-align: center;
+    width: 100%;
+    font-size: 30px;
+    padding-bottom: 50px;
   }
 `;
 
@@ -279,32 +333,36 @@ export const FourthBottomDiv = styled.div`
   justify-content: space-between;
   margin-top: 50px;
 
-  @media ${breakPoints.tablet} {
-    display: flex;
+  @media ${breakPoints.mobile} {
+    margin-top: 10px;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
   }
 `;
 
 export const FourthContents = styled.div`
   font-weight: normal;
-  font-size: 26px;
+  font-size: 20px;
   color: #0a0a0a;
-  line-height: 60px;
   letter-spacing: 3px;
 
-  @media ${breakPoints.tablet} {
-    font-weight: normal;
-    font-size: 26px;
-    text-align: end;
-    color: #0a0a0a;
-    line-height: 45px;
-    letter-spacing: 5px;
-    margin-top: 50px;
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    font-size: 15px;
+    padding-bottom: 20px;
   }
 `;
 
 export const FourthImage = styled.img`
   width: 500px;
+
+  @media ${breakPoints.mobile} {
+    max-width: 400px;
+    display: flex;
+    padding-top: 50px;
+  }
 `;
 
 export const BodyContents = styled(marginDiv)`
@@ -359,6 +417,10 @@ export const Footer = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #5b8dff;
+
+  @media ${breakPoints.mobile} {
+    height: 100px;
+  }
 `;
 
 export const DownLoadText = styled.div`
@@ -390,14 +452,29 @@ export const DownLoadButton = styled.button`
   align-items: center;
   justify-content: center;
   cursor: pointer;
+
+  @media ${breakPoints.mobile} {
+    max-width: 150px;
+    height: 50px;
+  }
 `;
 
 export const ButtonImage = styled.img`
   width: 40px;
   height: 40px;
+
+  @media ${breakPoints.mobile} {
+    width: 15px;
+    height: 15px;
+  }
 `;
 
-export const ButtonText = styled.text`
+export const ButtonText = styled.a`
   font-size: 20px;
   padding-left: 10px;
+  color: black;
+
+  @media ${breakPoints.mobile} {
+    font-size: 14px;
+  }
 `;
