a4a7f463867cdb21523a590262de32118326e18a
yeontan0826 <yeontan0826@gmail.com>
2022-07-26 15:05:20 +0900

    개인정보처리방침 약관, 서비스 이용 약관 페이지 생성

---
+++ pages/terms/service/index.tsx
@@ -0,0 +1,19 @@
+/* 서비스 이용약관 */
+
+import * as S from "../Terms.styles";
+
+export default function ServicePage() {
+  return (
+    <S.Wrapper>
+      <S.Title>서비스 이용약관 동의</S.Title>
+      <S.Box>
+        <S.Text isBold={true}>제 1조(목적)</S.Text>
+        <S.Text>
+          본 약관은 카픽이 제공하는 서비스의 이용과 관련하여 카픽과 회원과의
+          권리, 의무 및 책임사항, 기타 필요한 사항을 규정하는 것을 목적으로
+          합니다.
+        </S.Text>
+      </S.Box>
+    </S.Wrapper>
+  );
+}
